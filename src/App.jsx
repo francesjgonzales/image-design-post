1d0abe247d7f6cd0405a52ef6077bfa7366d0c91
francesjgonzales <francesj7837@gmail.com>
2024-11-30 23:49:48 -0700

    fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ export default function App() {
   return (
     /* using fragment <> can return multiple elements in a component */
     <>
-      <div className="m-2 p-2"></div>
-      <h1 className="md:container md:mx-auto bg-indigo-500 text-3xl box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 underline decoration-wavy">
-        Create your own fan photo!
-      </h1>
+      <div className="m-5 p-5">
+        <div className="text-center text-7xl m-8 pt-2 md:container md:mx-auto bg-indigo-500 box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-fuchsia-200 ">
+          <p className="underline decoration-wavy">
+            Create your own BTS photo!
+          </p>
+          <div className="text-center text-xl m-7 pb-3">
+            Make your own and save in an instant!
+          </div>
+        </div>
 
-      <MyComponent />
+        <MyComponent />
+      </div>
     </>
   );
 }
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -22,7 +22,13 @@ const MyComponent = () => {
   function handleImageChange(e) {
     setBtsMember({
       ...btsMember,
-      image: e.target.value,
+      image1: e.target.value,
+    });
+  }
+  function handleImageChange2(e) {
+    setBtsMember({
+      ...btsMember,
+      image2: e.target.value,
     });
   }
 
@@ -31,47 +37,77 @@ const MyComponent = () => {
     console.log(e.target.files);
     setFile(URL.createObjectURL(e.target.files[0]));
   }
+
   return (
     <>
       <div
         ref={componentRef}
         className="md:container md:mx-auto  bg-indigo-500"
       >
-        <h2>
-          {btsMember.name}
-          <br />
-        </h2>
+        <div className="object-none object-center">
+          <img height="650px" src={btsMember.image1} />
 
-        <img height="650px" src={btsMember.image} />
+          <img height="650px" src={btsMember.image2} />
+        </div>
 
         <img src={file} />
+        <h2 className="font-mono text-4xl pt-3 m-3 text-center">
+          {btsMember.name}
+        </h2>
       </div>
-      <div className="md:container md:mx-auto">
-        <input type="file" onChange={handleChange} />
-        <label>
-          Name:
-          <input value={btsMember.name} onChange={handleNameChange} />
-        </label>
-        <label>
-          Image:
+
+      <div class="mt-3 p-3 grid grid-cols-3 gap-4">
+        <div className="font-bold text-xl ">Upload your image</div>
+        <div className="col-span-2">
+          <input type="file" onChange={handleChange} />
+        </div>
+        <div className="font-bold text-xl ">Type your text</div>
+        <div className="col-span-2">
           <input
-            className="outline outline-offset-2 outline-blue-500"
-            value={btsMember.image}
+            className="p-1"
+            value={btsMember.name}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="font-bold text-xl ">Paste Image Address Link 1</div>
+        <div className="col-span-2">
+          <input
+            className="p-1"
+            value={btsMember.image1}
             onChange={handleImageChange}
           />
-        </label>
+        </div>
+        <div className="font-bold text-xl ">Paste Image Address Link 2</div>
+        <div className="col-span-2">
+          <input
+            className="p-1"
+            value={btsMember.image2}
+            onChange={handleImageChange2}
+          />
+        </div>
       </div>
+
       <div className="grid space-x-4 grid-cols-4 mt-6">
-        <button onClick={() => exportComponentAsJPEG(componentRef)}>
+        <button
+          className="bg-cyan-400	 text-blue-950"
+          onClick={() => exportComponentAsJPEG(componentRef)}
+        >
           Export As JPEG
         </button>
-        <button onClick={() => exportComponentAsPDF(componentRef)}>
+        <button
+          className="bg-cyan-400	 text-blue-950"
+          onClick={() => exportComponentAsPDF(componentRef)}
+        >
           Export As PDF
         </button>
-        <button onClick={() => exportComponentAsPNG(componentRef)}>
+        <button
+          className="bg-cyan-400	 text-blue-950"
+          onClick={() => exportComponentAsPNG(componentRef)}
+        >
           Export As PNG
         </button>
         <button
+          className="bg-cyan-400	 text-blue-950"
           type="button"
           onClick={() => {
             window.print();
